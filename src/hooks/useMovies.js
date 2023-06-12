c00f0d157a14b0c9d65865256d331aa535ff2fde
agustin cordero <agustin.cordero201@gmail.com>
2023-06-11 22:58:11 -0300

    no render 2 times the same result

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,17 +1,19 @@
-import withResults from '../mocks/with-results.json'
-import withoutResults from '../mocks/no-results.json'
-import { useState } from 'react'
+
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({search}) {
     const [movies, setMovies] =useState([])
     const [loading, setLoading] =useState(false)
     const [error,setError]= useState(null)
+    const previousSearch =useRef(search)
 
     const getMovies = async () =>{
+      if(search === previousSearch.current) return
     try{
       setLoading(true)
       setError(null)
+      previousSearch.current =search
       const newMovies =await searchMovies({search})
       setMovies(newMovies)
     }catch(e){
