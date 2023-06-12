3a16bba304fd8e0a98cbe6bf66e8f4bc7956ab5c
agustin cordero <agustin.cordero201@gmail.com>
2023-06-12 19:06:28 -0300

    patch finished

--- src/App.jsx
+++ src/App.jsx
@@ -37,17 +37,24 @@ function useSearch () {
 }
 
 function App() {
+  const [sort, setSort] =useState(false)
   const {search,updateSearch,error} =useSearch()
-  const {movies, loading, getMovies} =useMovies({search}) 
+  const {movies, loading, getMovies} =useMovies({search, sort}) 
 
 
  const handleSubmit = (e) =>{ 
     e.preventDefault()
-    getMovies()
+    getMovies({search})
    }
  
+const handleSort = () =>{
+  setSort(!sort)
+}
+
 const handleChange =(e) =>{
-  updateSearch(e.target.value)
+  const newSearch =e.target.value
+  updateSearch(newSearch)
+  getMovies({ search:newSearch })
 }
 
   return (
@@ -56,7 +63,9 @@ const handleChange =(e) =>{
         <h1>Movie searcher</h1>
       <form className="form" onSubmit={handleSubmit}>
          <input style={{border:'1px solid transparent',borderColor:error ? "red" : 'transparent'}}
-          onChange={handleChange} value={search} name='query' placeholder="Avengers, Star wars, The Matrix..." /> 
+          onChange={handleChange} value={search} name='query' placeholder="Avengers, Star wars, The Matrix..." 
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort}/>
          <button type='submit'>Search</button>
       </form>
       {error && <p style ={{color: 'red'}}>{error}</p>}
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,28 +1,32 @@
+import { useRef, useState, useMemo ,useCallback} from "react";
+import { searchMovies } from "../services/movies";
 
-import { useRef, useState } from 'react'
-import { searchMovies } from '../services/movies'
+export function useMovies({ search, sort }) {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const previousSearch = useRef(search);
 
-export function useMovies ({search}) {
-    const [movies, setMovies] =useState([])
-    const [loading, setLoading] =useState(false)
-    const [error,setError]= useState(null)
-    const previousSearch =useRef(search)
+  const getMovies = useCallback( async ({search}) => {
+      if (search === previousSearch.current) return;
+      try {
+        setLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+  },[]);
 
-    const getMovies = async () =>{
-      if(search === previousSearch.current) return
-    try{
-      setLoading(true)
-      setError(null)
-      previousSearch.current =search
-      const newMovies =await searchMovies({search})
-      setMovies(newMovies)
-    }catch(e){
-      setError(e.message)
-    }finally{
-      setLoading(false)
-    }
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
 
-    }
-  
-    return {movies, getMovies, loading}
-  }
+  return { movies: sortedMovies, getMovies, loading };
+}
